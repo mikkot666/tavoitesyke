ea5f700afba0b6576e65b6302b997888bd1e07d0
mikkot666 <t1tami02@students.oamk.fi>
2023-09-06 12:52:30 +0300

    loputkin filut puskettu App.js lisäksi

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState(0);
+  const [hrLowerLimit, setHrLowerLimit] = useState(0);
+  const [hrUpperLimit, setHrUpperLimit] = useState(0);
+
+  function change(text) {
+    setAge(text);
+    const lowerLimit = (220 - text) * 0.65;
+    const upperLimit = (220 - text) * 0.85;
+    setHrLowerLimit(lowerLimit);
+    setHrUpperLimit(upperLimit);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput style={styles.field} value={age} onChangeText={text => change(text)}
+      keyboardType='decimal-pad'/>
+      <Text style={styles.field}>Hr limits</Text>
+      <Text style={styles.field}>{hrLowerLimit.toFixed(0)} - {hrUpperLimit.toFixed(0)}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +29,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
   },
+  field: {
+    marginBottom: 10,
+  }
 });
